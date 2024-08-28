1437dfbbcae71351de00e0b842ea2c7cb60215f4
Andreas Martin Aanerud <a.m.aanerud@gmail.com>
2024-08-28 12:38:12 +0200

    Adding People fetch

--- fetchProfile.js
+++ fetchProfile.js
@@ -16,4 +16,24 @@ async function fetchProfile(accessToken) {
     }
 }
 
-module.exports = { fetchProfile };
+async function fetchPeopleIndex(accessToken) {
+    const options = {
+        headers: {
+            Authorization: `Bearer ${accessToken}`
+        },
+        params: {
+            $top: 50,
+            $select: 'displayName'
+        }
+    };
+
+    try {
+        const response = await axios.get(`${process.env.GRAPH_ENDPOINT}/v1.0/me/people`, options);
+        return response.data.value;
+    } catch (error) {
+        console.error('Error fetching people index:', error.response ? error.response.data : error.message);
+        throw error;
+    }
+}
+
+module.exports = { fetchProfile, fetchPeopleIndex };
