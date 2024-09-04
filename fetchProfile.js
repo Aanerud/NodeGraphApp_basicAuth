789d2ae84b0ca5e36cdbe3d7924ac6400db561ef
Andreas Martin Aanerud <a.m.aanerud@gmail.com>
2024-09-04 21:12:40 +0200

    Changeing to focus on ews calls

--- fetchProfile.js
+++ fetchProfile.js
@@ -16,35 +16,4 @@ async function fetchProfile(accessToken) {
     }
 }
 
-async function fetchPeopleSearch(accessToken) {
-    const options = {
-        headers: {
-            Authorization: `Bearer ${accessToken}`,
-            'Content-Type': 'application/json'
-        }
-    };
-
-    const body = {
-        requests: [
-            {
-                entityTypes: ["person"],
-                query: {
-                    queryString: "*"
-                },
-                from: 0,
-                size: 50,
-                fields: ["displayName"]
-            }
-        ]
-    };
-
-    try {
-        const response = await axios.post(`${process.env.GRAPH_ENDPOINT}/v1.0/search/query`, body, options);
-        return response.data.value[0].hitsContainers[0].hits.map(hit => hit.resource); // Extract the person objects from the response
-    } catch (error) {
-        console.error('Error fetching people using search:', error.response ? error.response.data : error.message);
-        throw error;
-    }
-}
-
-module.exports = { fetchProfile, fetchPeopleSearch };
+module.exports = { fetchProfile };
